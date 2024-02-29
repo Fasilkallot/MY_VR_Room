8abedd9827d84a9fbe8ad489e024ef67e1a83797
Muhammed Fasil K <fasilkalloth@gmail.com>
2024-02-29 18:01:49 +0530

    TV Control with remote added using interaction events

--- VR Room/Assets/_Course Library/Scripts/Actions/PlayVideo.cs
+++ VR Room/Assets/_Course Library/Scripts/Actions/PlayVideo.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -17,8 +19,16 @@ public class PlayVideo : MonoBehaviour
     [Tooltip("List of video clips to pull from")]
     public List<VideoClip> videoClips = new List<VideoClip>();
 
+    [Tooltip("Time for to hold the power button to turn on and off the TV")]
+    public float powerOnTime = 2.5f;
+
     private VideoPlayer videoPlayer = null;
     private MeshRenderer meshRenderer = null;
+
+    private bool powerButton = false;
+    private bool isTVOn = false;
+    private bool isFolding = false;
+    private float holdedTime = 0;
 
     private int index = 0;
 
@@ -51,6 +61,27 @@ public class PlayVideo : MonoBehaviour
         {
             Stop();
         }
+    }
+    private void Update()
+    {
+        if (powerButton)
+        {
+            holdedTime += Time.deltaTime;
+
+            if (holdedTime >= powerOnTime && !isFolding)
+            {
+                Stop();
+                TVPowerControl();
+            }
+
+        }
+        else if (isTVOn && holdedTime <= 1f && holdedTime > 0)
+        {
+            TogglePlayPause();
+            holdedTime = 0;
+        }
+        else holdedTime = 0;
+        
     }
 
     public void NextClip()
@@ -130,5 +161,47 @@ public class PlayVideo : MonoBehaviour
     {
         var mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
         videoMaterial = mat;
+    }
+
+    public void TVPowerControl()
+    {
+
+        // Define the target Y position based on whether the TV is on or off
+        float tvToPos = isTVOn ? 2.7f : 2.0f;
+
+        StopCoroutine(MoveTV(tvToPos));
+
+        StartCoroutine(MoveTV(tvToPos));
+
+    }
+
+    private IEnumerator MoveTV(float tvToPos)
+    {
+        isFolding = true;
+        Transform parent = transform.parent;
+
+
+        // Create the new position vector with the same X and Z coordinates but different Y coordinate
+        Vector3 newPosition = new Vector3(parent.position.x, tvToPos, parent.position.z);
+
+
+        while (Vector3.Distance(parent.position,newPosition) >= 0.01f)
+        {
+            // Move the parent GameObject towards the new position using Lerp
+            parent.position = Vector3.Lerp(parent.position, newPosition, 2f * Time.deltaTime); 
+            yield return null;
+        }
+        isFolding = false;
+        isTVOn = !isTVOn;
+
+    }
+
+    public void PowerButtonPressed()
+    {
+        powerButton = true;
+    }
+    public void PowerButtonReleased()
+    {
+        powerButton = false;
     }
 }
